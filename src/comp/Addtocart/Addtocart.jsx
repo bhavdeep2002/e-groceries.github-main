8edfececb3e569b056d16cc7cf77031ea416922e
bhavdeep2002 <78559880+bhavdeep2002@users.noreply.github.com>
2024-02-17 20:21:19 +0530

    update

--- src/comp/Addtocart/Addtocart.jsx
+++ src/comp/Addtocart/Addtocart.jsx
@@ -2,14 +2,16 @@ import React, { useContext } from 'react'
 import Addtocartbtn from '../Addtocartbtn/Addtocartbtn'
 import { Detail } from '../ProductDetail/ProductDetail'
 export default function Addtocart() {
-  const {name,price,description} =useContext(Detail)//useContext is a hook that can use context provided by compoment as specified
+  const {name,price,description} =useContext(Detail)//useContext is a hook that can use context provided by compoment 
+  // this can be used to provide only required data to a sepecific component
+  // if we use props then we should also contains all the props that is going to use in its child component
 
   return (
     <div className='name'>
       <h1 className='product_title' >{name}</h1>
       <p><b style={{ fontSize: '1.5rem', fontWeight: '700' }}>{price}</b>
       + Free Shipping <br />{description}</p>
-      <Addtocartbtn name={name}/>
+      <Addtocartbtn />
     </div>
   )
 }
--- src/comp/Addtocartbtn/Addtocartbtn.js
+++ src/comp/Addtocartbtn/Addtocartbtn.js
@@ -4,6 +4,9 @@ import { AlertMessage, Detail, FrontDetail } from '../ProductDetail/ProductDetai
 import axios from 'axios';
 
 export default function Addtocartbtn() {
+    // here i have used context if i use props, then this component's immedgate parent should contain all this props ,so if its immedgate parents have this then its grandparent should also have all this props ,so if i want to use props then i have to send props to all the child to have access to last grand child
+    
+    //but by using context hook i can sepecify which component should contain how much context/data ,this leads to components having only required information instead of having all the contexts
     const {setalert} = useContext(AlertMessage);
     const {name ,price} =useContext(Detail)
     const {image} =useContext(FrontDetail)
@@ -12,7 +15,7 @@ export default function Addtocartbtn() {
 
     useEffect(() => {
         // Check if item state is updated and not empty
-        if(isDataUpdated){
+        if(isDataUpdated){//this will not executed until/unless isDataUpdated is true, this protects unnessary rows in tablecart when ever Addtocartbtn has been rendered 
             axios.post("http://localhost:3012/cart", item)
             .then((res) => {
                 console.log(res);
@@ -22,7 +25,7 @@ export default function Addtocartbtn() {
             });
         }  
 
-    }, [isDataUpdated,item]);
+    }, [isDataUpdated,item]);// this will executed when ever from these two states has been changed
     
     function message() {
         swal({
@@ -39,8 +42,16 @@ export default function Addtocartbtn() {
         setIsDataUpdated(true)
     }
 
-    function quantity(){
-        
+    function quantity(name){
+        // Check if the item already exists in the list
+        axios.get("http://localhost:3012/cart")
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    // const existingItem = item.find(item => item.name === name);
     }
 
     return (
@@ -48,7 +59,7 @@ export default function Addtocartbtn() {
             <div className='col-md-12'>
                 <div>
                     <input
-                        onClick={() => { message(); quantity(itemName); }}
+                        onClick={() => { message(); quantity(name); }}
                         style={{ backgroundColor: "#6a9739" }}
                         className='btn btn-success addtocart'
                         type="submit"
