a5666ebc5c29e852b1dc9c3592a0d596cdf63a7b
bhavdeep2002 <78559880+bhavdeep2002@users.noreply.github.com>
2026-01-13 21:36:24 +0530

    Update port.js

--- src/comp/port/port.js
+++ src/comp/port/port.js
@@ -1,5 +1,5 @@
 const port="https://e-groceries-backend.onrender.com"
 const review="http://localhost:3012"
-const home="https://e-grocerer-b24v.onrender.com/"
+const home="https://e-grocerer-b24v.onrender.com"
 
 export {port,review,home}
